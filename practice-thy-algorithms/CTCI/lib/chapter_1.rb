45c8a2634d54090208950f4c186fdbe295482121
John Robin <johnrobinlugtu@gmail.com>
2018-01-08 14:44:18 -0800

    String Compression

--- practice-thy-algorithms/CTCI/lib/chapter_1.rb
+++ practice-thy-algorithms/CTCI/lib/chapter_1.rb
@@ -73,3 +73,32 @@ def one_away?(str, edit_str)
 
     edit_counter > 1 ? false : true
 end
+
+# 1.6 String Compression: Implement a method to perform basic string compression
+# using the counts of repeated characters. 
+# For example, the string aabcccccaaa => a2b1c5a3.
+# If the "compressed" string would not become smaller than the original string,
+# your method should return the original string. 
+# You can assume the string has only uppercase and lowercase letters(a - z)
+
+def string_compression(str)
+    # step 1 create variables
+    result = ""
+    counter = 0
+    cur_char = ""
+    
+    str.each_char do |ch|
+        if cur_char != ch
+            result += cur_char
+            result += counter.to_s if counter != 0
+
+            cur_char = ch
+            counter = 1
+        elsif cur_char == ch
+            counter += 1
+        end
+    end
+
+    result = counter != 0 ? result + cur_char + counter.to_s : ""
+end
+
--- practice-thy-algorithms/CTCI/spec/chapter_1_spec.rb
+++ practice-thy-algorithms/CTCI/spec/chapter_1_spec.rb
@@ -11,5 +11,11 @@ describe 'one_away' do
     specify { expect(one_away?("super", "supen")). to be true}
     specify { expect(one_away?("hey there", "hey there")).to be true}
     specify { expect(one_away?("cake", "bart")).to be false}
+end
 
+describe 'string_compression' do 
+    specify { expect(string_compression("aaabbbccaa")).to eq("a3b3c2a2")}
+    specify { expect(string_compression("abc")).to eq("a1b1c1")}
+    specify { expect(string_compression("daawwwwggg")).to eq("d1a2w4g3")}
+    specify { expect(string_compression("")).to eq("")}
 end
