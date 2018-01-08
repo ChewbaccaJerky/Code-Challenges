b3558557aac8051870a345ee27ac1f4bfa32a546
John Robin <johnrobinlugtu@gmail.com>
2018-01-08 00:44:38 -0800

    found optimized productify

--- practice-thy-algorithms/Ruby/lib/algorithms.rb
+++ practice-thy-algorithms/Ruby/lib/algorithms.rb
@@ -258,9 +258,31 @@ end
 # Replace all the numbers with the product of all other numbers.
 # Do this in O(n) time without using division.
 def productify(array)
-    product = array.reduce(:*)
-    puts product
-    array.map {|n| product / n}
+    len = array.length
+    left = Array.new(len){1}
+    right = Array.new(len){1}
+
+    # step 1 create left array O(n)
+    array.each_index do |i|
+        next if i == 0
+        left[i] = left[i - 1] * array[i - 1]
+    end
+    
+    # step 2 create right array O(n)
+    array.each_index do |i|
+        next if i == 0
+        right[len - i - 1] = array[len - i] * right[len - i]
+    end
+
+    # step 3 multiply products O(n)
+    array.each_index do |i|
+        array[i] = left[i] * right[i]
+    end
+
+    # step 4 return results
+    array
+
+    # time complexity O(n + n + n) => O(n)
 end
 
 # Write a function that takes an array and returns all of its subsets.
