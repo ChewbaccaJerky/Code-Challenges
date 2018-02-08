d0417ae7c05d3e7819fd9f4a09498603b0bc52d9
John Robin <johnrobinlugtu@gmail.com>
2018-02-07 23:02:50 -0800

    Longest Palindrome O(n**2) Time complexity

--- practice-thy-algorithms/Leetcode/longest_palindrome.rb
+++ practice-thy-algorithms/Leetcode/longest_palindrome.rb
@@ -9,17 +9,22 @@ def longest_palindrome(str)
         # Step 2 Check if adjacent chars are equal
         left = i
         if str[left] == str[left + 1]
-            right = (left + 1)
+            right = left + 1
+            while str[left] == str[right]
+                right += 1
+            end
+            right -= 1
+
             # expand until not a palindrome
             until str[left] != str[right]
-                longest = str[left..right]
+                longest = str[left..right] if str[left..right].length > longest.length
                 left -= 1
                 right += 1
             end
         elsif str[left] == str[left + 2]
             right = (left + 2)
             until str[left] != str[right]
-                longest = str[left..right]
+                longest = str[left..right] if str[left..right].length > longest.length
                 left -= 1
                 right += 1
             end
@@ -27,11 +32,13 @@ def longest_palindrome(str)
         i += 1
     end
 
-    longest
+    longest == "" ? str[0] : longest
 end
 
 describe "longest_palindrome" do
     specify{ expect(longest_palindrome("abadc")).to eq("aba") }
     specify{ expect(longest_palindrome("adgracecar")).to eq("racecar") }
-    specify{ expect(longest_palindrome("acdbg")).to eq("") }
+    specify{ expect(longest_palindrome("acdbg")).to eq("a") }
+    specify{ expect(longest_palindrome("ccc")).to eq("ccc") }
+    specify{ expect(longest_palindrome("cccc")).to eq("cccc") }
 end
