a9f4687f04fa457f8edfad17063f4e56e64b5e3d
John Robin <johnrobinlugtu@gmail.com>
2018-02-07 22:37:36 -0800

    Longest Palindrome O(n**2)

---
+++ practice-thy-algorithms/Leetcode/longest_palindrome.rb
@@ -0,0 +1,37 @@
+
+def longest_palindrome(str)
+    longest = ""
+
+    i = 0
+    # Step 1 iterate through each char
+    while i < str.length
+        j = 0
+        # Step 2 Check if adjacent chars are equal
+        left = i
+        if str[left] == str[left + 1]
+            right = (left + 1)
+            # expand until not a palindrome
+            until str[left] != str[right]
+                longest = str[left..right]
+                left -= 1
+                right += 1
+            end
+        elsif str[left] == str[left + 2]
+            right = (left + 2)
+            until str[left] != str[right]
+                longest = str[left..right]
+                left -= 1
+                right += 1
+            end
+        end
+        i += 1
+    end
+
+    longest
+end
+
+describe "longest_palindrome" do
+    specify{ expect(longest_palindrome("abadc")).to eq("aba") }
+    specify{ expect(longest_palindrome("adgracecar")).to eq("racecar") }
+    specify{ expect(longest_palindrome("acdbg")).to eq("") }
+end
