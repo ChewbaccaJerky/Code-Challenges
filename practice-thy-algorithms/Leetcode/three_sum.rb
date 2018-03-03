c97347d55e76186f3b6018182bed7eb3fb62f22a
John Robin <johnrobinlugtu@gmail.com>
2018-03-02 20:46:39 -0800

    Three Sum Problem

--- practice-thy-algorithms/Leetcode/three_sum.rb
+++ practice-thy-algorithms/Leetcode/three_sum.rb
@@ -21,3 +21,56 @@ def naive_three_sum(nums)
     end
 end
 
+def three_sum_slow(nums)
+    combo = nums.combination(3).to_a
+    (combo.map!{|arr| arr.sort }).select{ |arr| arr.reduce(:+) == 0 }.uniq
+end
+
+def three_sum_double_pointer(nums)
+    nums.sort! # O(nLogn)
+    l = 0
+    r = nums.length - 1
+    result = []    
+end
+
+# def three_sum(nums)
+#     perm = nums.combination(3).to_a
+#     result = perm.select do |nums|
+#         if nums.reduce(:+) == 0
+#     end
+
+#     result
+# end
+
+def three_sums(nums)
+    nums.sort!
+    l = 0
+    r = nums.length - 1
+    result = []
+
+    i = 0
+    until r <= l
+        search_val = (nums[l] + nums[r]) * -1
+
+        (result << [nums[l], nums[r], search_val]) if rest?(nums, l, r)
+        if i % 2
+            l += 1
+        else
+            r -= 1
+        end
+        i += 1
+    end
+
+    result.uniq
+end
+
+def rest?(nums, l, r)
+    val = (nums[l] + nums[r]) * -1
+    nums.each.with_index do |num, idx|
+        return true if val == num && idx != l && idx != r
+    end
+    false
+end
+
+arr = [-1, 0, -1, 1, 2]
+print three_sums(arr)
