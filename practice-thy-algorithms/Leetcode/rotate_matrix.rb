463ae2ff1cef197d3e14390896aab92b29fd6aea
John Robin <johnrobinlugtu@gmail.com>
2018-03-27 19:07:59 -0700

    added in place rotation

--- practice-thy-algorithms/Leetcode/rotate_matrix.rb
+++ practice-thy-algorithms/Leetcode/rotate_matrix.rb
@@ -20,3 +20,48 @@ def rotate(matrix)
     
     print matrix
 end
+
+def rotate_in_place(mat)
+    max = mat.length
+    x = 0
+    while x < mat.length / 2
+        y = x
+
+        while y < mat.length - 1
+            # store current cell in temp variable
+            temp = mat[x][y];
+
+            # move values from bottom-left to top-left
+            mat[x][y] = mat[max-1-y][x]
+
+            # move values from bottom-right to bottom-left
+            mat[max-1-y][x] = mat[max-1-x][max-1-y]
+
+            # move values from top-right to bottom-right
+            mat[max-1-x][max-1-y] = mat[x][max-1-y]
+
+            # move values from temp to top-right
+            mat[y][max-1-y] = temp
+
+            y += 1
+        end
+        x += 1
+    end
+
+    mat
+end
+
+describe "rotate_in_place" do
+    matrix = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
+    ]
+
+    result = [
+        [7,4,1],
+        [8,5,2],
+        [9,6,3]
+    ]
+    specify { expect(rotate_in_place(matrix)).to eq(result)}
+end
