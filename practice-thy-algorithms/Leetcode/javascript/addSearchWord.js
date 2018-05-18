75e5c55bc0867a58a43955ea3957c88759de2db6
John Robin <johnrobinlugtu@gmail.com>
2018-05-18 12:30:20 -0700

    write search word

---
+++ practice-thy-algorithms/Leetcode/javascript/addSearchWord.js
@@ -0,0 +1,57 @@
+/*
+Design a data structure that supports the following two operations:
+
+void addWord(word)
+bool search(word)
+search(word) can search a literal word or a regular expression string containing only letters a-z or .. A . means it can represent any one letter.
+
+Example:
+
+addWord("bad")
+addWord("dad")
+addWord("mad")
+search("pad") -> false
+search("bad") -> true
+search(".ad") -> true
+search("b..") -> true
+Note:
+You may assume that all words are consist of lowercase letters a-z.
+*/
+
+/**
+ * Initialize your data structure here.
+ */
+var WordDictionary = function() {
+    this.stack = [];
+};
+
+/**
+ * Adds a word into the data structure. 
+ * @param {string} word
+ * @return {void}
+ */
+WordDictionary.prototype.addWord = function(word) {
+    this.stack.push(word);
+};
+
+/**
+ * Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. 
+ * @param {string} word
+ * @return {boolean}
+ */
+WordDictionary.prototype.search = function(word) {
+    const regex = new RegExp(`^${word}$`);
+    
+    for(let i = 0; i < this.stack.length; i++) {
+        if(regex.test(this.stack[i])) return true;
+    }
+    
+    return false;
+};
+
+/** 
+ * Your WordDictionary object will be instantiated and called as such:
+ * var obj = Object.create(WordDictionary).createNew()
+ * obj.addWord(word)
+ * var param_2 = obj.search(word)
+ */
