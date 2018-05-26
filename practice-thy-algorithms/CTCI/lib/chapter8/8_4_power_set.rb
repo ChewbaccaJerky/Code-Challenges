97e0dbc1e1bdf82021fce0e6bbbed2bbc8d3fb9a
John Robin <johnrobinlugtu@gmail.com>
2018-05-25 23:20:26 -0700

    subset with recursion

--- practice-thy-algorithms/CTCI/lib/chapter8/8_4_power_set.rb
+++ practice-thy-algorithms/CTCI/lib/chapter8/8_4_power_set.rb
@@ -1,9 +1,14 @@
+=begin
+ 8.4 CTCI Power Set
+ 
+ Write a method to return all subsets of a set
 
-# CTCI Power Set 8.4
+=end
 
+# Binary Solution
 def power_set(subset)
   result = []
-  max_length = (2 ** (subset.length)) - 1
+  max_length = (2 ** (subset.length)) + 1
   
   max_length.times do |i|
    sub = convert_to_subset(i, subset)
@@ -26,3 +31,16 @@ def convert_to_subset(num, subset)
 
   sub
 end
+
+# Recursive Solution
+
+def recursive_power_set(subset)
+  return [[]] if subset.empty?
+
+  sub = recursive_power_set(subset[1..-1])
+
+  char = subset[0]
+  sub + sub.map {|arr| arr + [char]}
+end
+
+print recursive_power_set(['a', 'k', 'd'])
